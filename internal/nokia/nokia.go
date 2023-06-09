21fd7f23c12792eff63c2548b8c1574e576c223b
Greg Dennis <5436032+greg-dennis@users.noreply.github.com>
2023-06-09 10:30:46 -0400

    Remove "Nokia" from function godocs

--- internal/nokia/nokia.go
+++ internal/nokia/nokia.go
@@ -28,7 +28,7 @@ type Namer struct {
 	HardwareModel string
 }
 
-// LoopbackInterface is a Nokia implementation of namer.LoopbackInterface.
+// LoopbackInterface is an implementation of namer.LoopbackInterface.
 func (n *Namer) LoopbackInterface(index int) (string, error) {
 	const maxIndex = 255
 	if index > maxIndex {
@@ -37,7 +37,7 @@ func (n *Namer) LoopbackInterface(index int) (string, error) {
 	return fmt.Sprintf("lo%d", index), nil
 }
 
-// AggregateInterface is a Nokia implementation of namer.AggregateInterface.
+// AggregateInterface is an implementation of namer.AggregateInterface.
 func (n *Namer) AggregateInterface(index int) (string, error) {
 	const maxIndex = 127
 	if index > maxIndex {
@@ -46,7 +46,7 @@ func (n *Namer) AggregateInterface(index int) (string, error) {
 	return fmt.Sprintf("lag%d", index+1), nil
 }
 
-// AggregateMemberInterface is a Nokia implementation of namer.AggregateMemberInterface.
+// AggregateMemberInterface is an implementation of namer.AggregateMemberInterface.
 func (n *Namer) AggregateMemberInterface(index int) (string, error) {
 	name, err := n.AggregateInterface(index)
 	if err != nil {
@@ -55,7 +55,7 @@ func (n *Namer) AggregateMemberInterface(index int) (string, error) {
 	return name + ".0", nil
 }
 
-// Linecard is a Nokia implementation of namer.Linecard.
+// Linecard is an implementation of namer.Linecard.
 func (n *Namer) Linecard(index int) (string, error) {
 	const maxIndex = 7
 	if index > maxIndex {
@@ -64,7 +64,7 @@ func (n *Namer) Linecard(index int) (string, error) {
 	return fmt.Sprintf("Linecard%d", index+1), nil
 }
 
-// ControllerCard is a Nokia implementation of namer.ControllerCard.
+// ControllerCard is an implementation of namer.ControllerCard.
 func (n *Namer) ControllerCard(index int) (string, error) {
 	const maxIndex = 1
 	if index > maxIndex {
@@ -73,7 +73,7 @@ func (n *Namer) ControllerCard(index int) (string, error) {
 	return fmt.Sprintf("Supervisor%d", index+1), nil
 }
 
-// Fabric is a Nokia implementation of namer.Fabric.
+// Fabric is an implementation of namer.Fabric.
 func (n *Namer) Fabric(index int) (string, error) {
 	const maxIndex = 7
 	if index > maxIndex {
